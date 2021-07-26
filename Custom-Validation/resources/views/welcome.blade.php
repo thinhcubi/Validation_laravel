c78d1d4bb81b4bd46501d42fea3c6094ffa1606c
thinhcubi <thinhcubi27@gmail.com>
2021-07-26 15:19:22 +0700

    Custom-Validation

---
+++ Custom-Validation/resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<div class="content">
+    <div class="title m-b-md">
+        Laravel Cusstom Validation
+    </div>
+
+        <form action="{{ route('form.submit') }}" method="GET">
+            <label for="name">Name: </label>
+            <input id="name" name="name" type="text" placeholder="Enter the full name">
+            <br>
+            <label for="age">Age: </label>
+            <input id="age" name="age" type="text" placeholder="Enter the age">
+            <br>
+            <button type="submit">Submit</button>
+        </form>
+    <p style='color:green'>{{ (isset($success)) ? $success : '' }}</p>
+    <div class="form-custom-validation">
+        <div class="error-message">
+            @if ($errors->any())
+                @foreach($errors->all() as $nameError)
+                    <p style="color:red">{{ $nameError }}</p>
+                @endforeach
+            @endif
+        </div>
+    </div>
+</div>
